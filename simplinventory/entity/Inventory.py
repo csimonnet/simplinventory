7e78de8ccaca0feb2a35d0916884f29a92313a6b
Charlotte Simonnet <charlotte.simonnet@port053.abc-objectif.local>
2021-01-24 13:48:43 +0100

    Defining basic entities

---
+++ simplinventory/entity/Inventory.py
@@ -0,0 +1,10 @@
+class Inventory:
+
+    def __init__(self, items=[]):
+        self.items = items
+
+    def add_item(self, item):
+        return self
+
+    def remove_item(self, item):
+        return self;
---
+++ simplinventory/entity/KitchenInventory.py
@@ -0,0 +1,5 @@
+import Inventory
+
+
+class KitchenInventory(Inventory):
+    pass
